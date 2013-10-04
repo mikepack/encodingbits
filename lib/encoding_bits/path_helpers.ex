a3aa684b4576f9834384f63fbcc99aee89c414e0
Mike Pack <mikepackdev@gmail.com>
2013-10-04 13:39:23 -0600

    Fix dev paths

--- lib/encoding_bits/path_helpers.ex
+++ lib/encoding_bits/path_helpers.ex
@@ -1,13 +1,13 @@
 defmodule EncodingBits.PathHelpers do
   def raw_articles_path do
-    paths = [dev: Path.expand("../articles/published", :code.priv_dir(:encoding_bits)),
+    paths = [dev: Path.expand("../../../../articles/published", :code.priv_dir(:encoding_bits)),
              test: Path.expand("../tmp/test/articles", :code.priv_dir(:encoding_bits)),
              prod: Path.expand("../articles/published", :code.priv_dir(:encoding_bits))]
     paths[EncodingBits.Dynamo.config[:dynamo][:env]]
   end
 
   def published_articles_path do
-    paths = [dev: Path.expand("published_articles", :code.priv_dir(:encoding_bits)),
+    paths = [dev: Path.expand("../../../../priv/published_articles", :code.priv_dir(:encoding_bits)),
              test: Path.expand("../tmp/test/published_articles", :code.priv_dir(:encoding_bits)),
              prod: Path.expand("published_articles", :code.priv_dir(:encoding_bits))]
     paths[EncodingBits.Dynamo.config[:dynamo][:env]]
