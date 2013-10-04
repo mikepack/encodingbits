22f3c94bf0a37094cab649e7ceb31058fc23b84b
Mike Pack <mikepackdev@gmail.com>
2013-10-04 13:09:54 -0600

    Using quotes for production?

--- lib/encoding_bits/path_helpers.ex
+++ lib/encoding_bits/path_helpers.ex
@@ -1,15 +1,19 @@
 defmodule EncodingBits.PathHelpers do
   def raw_articles_path do
-    paths = [dev: Path.expand("../../../articles/published", __FILE__),
-             test: Path.expand("../../../tmp/test/articles", __FILE__),
-             prod: Path.expand("../../../articles/published", __FILE__)]
+    paths = unquote(quote do
+      [dev: Path.expand("../../../articles/published", __FILE__),
+       test: Path.expand("../../../tmp/test/articles", __FILE__),
+       prod: Path.expand("../../../articles/published", __FILE__)]
+    end)
     paths[EncodingBits.Dynamo.config[:dynamo][:env]]
   end
 
   def published_articles_path do
-    paths = [dev: Path.expand("../../../priv/published_articles", __FILE__),
-             test: Path.expand("../../../tmp/test/published_articles", __FILE__),
-             prod: Path.expand("../../../priv/published_articles", __FILE__)]
+    paths = unquote(quote do
+      [dev: Path.expand("../../../priv/published_articles", __FILE__),
+       test: Path.expand("../../../tmp/test/published_articles", __FILE__),
+       prod: Path.expand("../../../priv/published_articles", __FILE__)]
+    end)
     paths[EncodingBits.Dynamo.config[:dynamo][:env]]
   end
 end
