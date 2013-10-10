f8fe69a2c488955581d35184f54d25f466aa7ce4
Mike Pack <mikepackdev@gmail.com>
2013-10-10 17:02:54 -0600

    Add datetime helper

--- lib/encoding_bits/helpers.ex
+++ lib/encoding_bits/helpers.ex
@@ -7,11 +7,15 @@ defmodule EncodingBits.Helpers do
     end
   end
 
+  defmacro datetime(article) do
+    quote do
+      a = unquote(article)
+      "#{a[:year]}-#{String.rjust(to_string(a[:month]), 2, ?0)}-#{String.rjust(to_string(a[:day]), 2, ?0)}T00:00:00-07:00"
+    end
+  end
+
   def title(content) when is_bitstring(content) do
     " - #{content}"
   end
-
-  def title(content) do
-    ""
-  end
+  def title(_) do "" end
 end
